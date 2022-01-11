0f94c84dea644ecf4bcf15aff608367fc2ff49af
Lucas Lee <luclee@ucsc.edu>
2022-01-11 05:12:12 +0000

    adding plot.sh finished maximum graph

--- asgn1/plot.sh
+++ asgn1/plot.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
 rm -f /tmp/length.dat
+rm -f /tmp/values.dat
+rm -f /tmp/maxvalues.dat
 
 for n in {2..10000};do
     echo -n "$n " >> /tmp/length.dat
     ./collatz -n $n | wc -l >> /tmp/length.dat
- 
+    echo -n "$n " >> /tmp/values.dat
+    ./collatz -n $n | sort -nr | head -n 1  >> /tmp/values.dat
+      
 done
+
+#sort -nr /tmp/values.dat >> /tmp/maxvalues.dat
 
 gnuplot <<END
     set terminal pdf
@@ -15,6 +21,18 @@ gnuplot <<END
     set xlabel "n"
     set ylabel "length"
     set zeroaxis
-    plot "/tmp/length.dat" with points title "" 
+    plot "/tmp/length.dat" with dots
+END
+
+gnuplot <<END
+    set terminal pdf
+    set output "values.pdf"
+    set title "Maximum Collatz Sequencce Value"
+    set xlabel"n"
+    set ylabel "value"
+    set xrange [0:10000]
+    set yrange [0:100000]
+    set zeroaxis
+    plot "/tmp/values.dat" with dots 
 END
 
