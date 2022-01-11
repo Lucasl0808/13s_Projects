4010b42c8ad40be2bc8520f73f6ae7ce01dfc65d
Lucas Lee <luclee@ucsc.edu>
2022-01-11 06:46:59 +0000

    working on histograms

--- asgn1/plot.sh
+++ asgn1/plot.sh
@@ -2,17 +2,17 @@
 
 rm -f /tmp/length.dat
 rm -f /tmp/values.dat
-rm -f /tmp/maxvalues.dat
+rm -f /tmp/histogram.dat
 
 for n in {2..10000};do
     echo -n "$n " >> /tmp/length.dat
     ./collatz -n $n | wc -l >> /tmp/length.dat
     echo -n "$n " >> /tmp/values.dat
     ./collatz -n $n | sort -nr | head -n 1  >> /tmp/values.dat
-      
+    ./collatz -n $n | wc -l >> /tmp/histogram.dat
 done
 
-#sort -nr /tmp/values.dat >> /tmp/maxvalues.dat
+cat /tmp/histogram.dat | uniq -c | less  >> /tmp/histograms.dat
 
 gnuplot <<END
     set terminal pdf
@@ -28,11 +28,22 @@ gnuplot <<END
     set terminal pdf
     set output "values.pdf"
     set title "Maximum Collatz Sequencce Value"
-    set xlabel"n"
+    set xlabel "n"
     set ylabel "value"
     set xrange [0:10000]
     set yrange [0:100000]
     set zeroaxis
     plot "/tmp/values.dat" with dots 
+END
+
+gnuplot <<END
+    set terminal pdf
+    set output "histogram.pdf"
+    set title "Collatz Sequence Length Histogram"
+    set xlabel "length"
+    set ylabel "frequency"
+    set xrange [0:225]
+    set yrange [0:200]
+    plot "/tmp/histograms.dat" using 2:xticlabels(1) with histograms
 END
 
