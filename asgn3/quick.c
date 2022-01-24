8faa8c6cf60b607568d771e2c221f08175fdcbbd
Lucas Lee <luclee@ucsc.edu>
2022-01-24 23:09:03 +0000

    finishing quicksort and testing it in test file

--- asgn3/quick.c
+++ asgn3/quick.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 #include "stats.h"
 
-Stats quick_stats;
+//Stats quick_stats;
 
-int partition(uint32_t A[], int low, int high){
+int partition(Stats *quick_stats, uint32_t A[], int low, int high){
 	int i = low -1;
-	for(int j = low; j < high, j ++){
-		if(cmp(&quick_stats,A[j - 1], A[high - 1]) < 0){
+	for(int j = low; j < high; j +=1){
+		if(cmp(quick_stats, A[j - 1], A[high - 1]) < 0){
 			i += 1;
-			int temp = A[i - 1];
-			A[i - 1] = A[j - 1];
-			A[j - 1] = temp;
+			swap(quick_stats, &A[i-1], &A[j-1]);
+			//int temp = A[i - 1];
+			//A[i - 1] = A[j - 1];
+			//A[j - 1] = temp;
 		}
 	}
-	swap(&quick_stats, A[i], A[high -1]);
+	swap(quick_stats, &A[i], &A[high -1]);
 
 	//int temp2 = A[i];
 	//A[i] = A[high - 1];
@@ -21,14 +22,14 @@ int partition(uint32_t A[], int low, int high){
 	return i + 1;
 }
 
-void quick_sorter(uint32_t A[], int low, int high){
-	if(cmp(&quick_stats,low, high) < 0){
-		int p = partition(A, low, high);
-		quick_sorter(A, low, p - 1);
-		quick_sorter(A, p + 1, high);
+void quick_sorter(Stats *quick_stats, uint32_t A[], int low, int high){
+	if(low < high){
+		int p = partition(quick_stats,A, low, high);
+		quick_sorter(quick_stats, A, low, p - 1);
+		quick_sorter(quick_stats, A, p + 1, high);
 	}
 }
 
-void quick_sort(Stats *stats, uint32_t A[], int n){
-	quick_sorter(A, 1, n);
+void quick_sort(Stats *quick_stats, uint32_t A[], int n){
+	quick_sorter(quick_stats, A, 1, n);
 }
--- asgn3/test.c
+++ asgn3/test.c
@@ -1,8 +1,14 @@
 #include "stats.h"
 #include <stdio.h>
+#include "quick.h"
+void printarray(uint32_t A[], int n){
+	for(int i = 0; i < n; i += 1){
+		printf("%d ", A[i]);
+	}
+}
 
 int main(void){
-	Stats record;
+	/*Stats record;
 	reset(&record);
 	uint32_t A[5] = {2,3,4,5,6};
 	if (cmp(&record, A[4], A[2]) < 0){
@@ -13,5 +19,12 @@ int main(void){
 		printf("4 > 2");
 
 	}
+	*/
+	Stats quick_stats;
+	reset(&quick_stats);
+	uint32_t B[5] = {4,7,1,8,5};
+	quick_sort(&quick_stats, B, 5);
+	printarray(B, 5);
+	printf("moves = %lu\n compares = %lu\n",quick_stats.moves, quick_stats.compares); 
 	return 1;
 }
