645b591b00e26b4fb8764062330124eaea1660c3
Lucas Lee <luclee@ucsc.edu>
2022-01-25 05:00:49 +0000

    adding header for quicksort

---
+++ asgn3/quick.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "stats.h"
+
+void partition(uint32_t *A, int l, int h);
+
+void quick_sorter(uint32_t *A, int l, int h);
+
+void quick_sort(Stats *stats, uint32_t *A, uint32_t n);
+
+
