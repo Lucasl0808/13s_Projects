2c75ca94e527aa1e8c0582417583b6d7f4dc0b17
Lucas Lee <luclee@ucsc.edu>
2022-01-25 11:24:21 +0000

    adding finished batcher sort

--- asgn3/batcher.c
+++ asgn3/batcher.c
@@ -1,3 +1,40 @@
 #include <stdio.h>
+#include "stats.h"
 
+void comparator(Stats *batcher_stats, uint32_t A[], int x, int y){
+	if(cmp(batcher_stats, A[x], A[y]) > 0){
+		swap(batcher_stats, &A[x], &A[y]);
+	}
+}
 
+void batcher_sort(Stats *batcher_stats, uint32_t A[], int n){
+	if(n == 0){
+		return;
+	}
+	int count = 0;
+	int temp_n = n;
+	while(temp_n > 0){
+		temp_n /= 2;
+		count += 1;
+	}
+	int t = count;
+	int p = 1 << (t - 1);
+	while(p > 0){
+		int q = 1 << (t - 1);
+		int r = 0;
+		int d = p;
+		
+		while( d > 0){
+			for(int i =0; i < n-d; i += 1){
+				if((i & p) == r){
+					comparator(batcher_stats, A, i, i +d);
+				}
+			}
+			d = q - p;
+			q = q >> 1;
+			r = p;	
+		}
+		p = p >> 1;
+	}
+
+}
---
+++ asgn3/batcher.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "stats.h"
+
+void comparator(Stats *stats, uint32_t *A, int x, int y);
+
+void batcher_sort(Stats *stats, uint32_t *A, uint32_t n);
+
