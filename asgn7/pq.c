ca22661298deab9afe24f7d6c34f8149af6b0eaf
Lucas Lee <luclee@ucsc.edu>
2022-03-10 09:49:23 +0000

    working pq using insertion

--- asgn7/pq.c
+++ asgn7/pq.c
@@ -25,12 +25,12 @@ PQindex index_create(char *author, double dist){
 	PQindex.dist = dist;
 	return PQindex;
 }
-
+/*
 void index_delete(PQindex **PQindex){
 	free(*PQindex);
 	*PQindex = NULL;
 }
-
+*/
 PriorityQueue *pq_create(uint32_t capacity){
 	PriorityQueue *q = (PriorityQueue *)malloc(sizeof(PriorityQueue));
 	q->capacity = capacity;
@@ -88,26 +88,57 @@ bool enqueue(PriorityQueue *q, char *author, double dist){
 	return true;
 }
 
+bool dequeue(PriorityQueue *q, char **author, double *dist){
+	if(pq_empty(q)){
+		return false;
+	}
+	*author = q->array[0].author;
+	*dist = q->array[0].dist;
+	for(uint32_t i = 1; i < pq_size(q); i +=1){
+		q->array[i-1] = q->array[i];
+		if(i == pq_size(q)){
+			q->array[i].author = NULL;
+			q->array[i].dist = 0;
+		}
+	}
+	q->size -= 1;
+	return true;
+}
+
 uint32_t pq_size(PriorityQueue *q){
 	return q->size;
 }
 
+/*
 void index_print(PQindex *PQindex){
 	printf("author = %s\n", PQindex->author);
 	printf("distance = %.15f\n", PQindex->dist);
 }
 
+*/
 void pq_print(PriorityQueue *q){
 	for(uint32_t i = 0; i < pq_size(q); i += 1){
 		printf("index author = %s, dist = %.15f\n", q->array[i].author, q->array[i].dist);
 	}
 }
+
+/*
 int main(void){
 	PriorityQueue *q = pq_create(4);
 	PQindex one = index_create("Kanye", 0.0023);
 	one.dist = 0.0245;
+	PQindex two = index_create("Jay-Z", .1);
+	PQindex three = index_create("21 Savage", .0002);
 	index_print(&one);
+	enqueue(q, two.author, two.dist);
 	enqueue(q, one.author, one.dist);
+	enqueue(q, three.author, three.dist);
+	char *a;
+	double d = 0;
+	dequeue(q, &a, &d);
+	printf("dequeue author = %s, distance = %.15f\n", a, d);
 	pq_print(q);
+	pq_delete(&q);
 	return 1;
 }
+*/
